20e041f9016ef0dc932242880f66daef29e26e4b
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-08 15:11:05 +0200

    adding more resources

--- Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -2,7 +2,16 @@ package lastpencil;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Number of pencils on the table
+        int numberOfPencils = 7;
 
+        // Print pencils using vertical bar symbols
+        for (int i = 0; i < numberOfPencils; i++) {
+            System.out.print("|");
+        }
+
+        // Print the next line for "Your turn!" prompt
+        System.out.println();
+        System.out.println("Your turn!");
     }
 }
