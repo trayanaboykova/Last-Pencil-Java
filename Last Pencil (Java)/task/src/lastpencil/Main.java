b0c15811bd8bf0386e1cce84c56205cff4ddbb21
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-08 15:26:39 +0200

    adding project stage

--- Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -1,17 +1,29 @@
 package lastpencil;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Number of pencils on the table
-        int numberOfPencils = 7;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many pencils would you like to use?");
+        int number = scanner.nextInt();
+        String firstUser = "John";
+        String secondUser = "Jack";
+        System.out.println("Who will be the first (John, Jack)");
+        String userInput = scanner.next();
 
         // Print pencils using vertical bar symbols
-        for (int i = 0; i < numberOfPencils; i++) {
+        for (int i = 0; i < number; i++) {
             System.out.print("|");
         }
 
         // Print the next line for "Your turn!" prompt
         System.out.println();
-        System.out.println("Your turn!");
+        if (firstUser.equals(userInput)) {
+            System.out.printf("%s is going first\n", firstUser);
+        } else {
+            System.out.printf("%s is going first\n", secondUser);
+        }
     }
 }
